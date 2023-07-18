514f76dc0d9326cea604a098299ef87b2f4222e1
mananuf <bmananaf@gmail.com>
2023-07-18 19:38:34 +0100

    test fixtures

---
+++ test_fixtures/tests/test_account.py
@@ -0,0 +1,51 @@
+"""
+Test Cases TestAccountModel
+"""
+import json
+from unittest import TestCase
+from models import db
+from models.account import Account
+
+ACCOUNT_DATA = {}
+
+class TestAccountModel(TestCase):
+    """Test Account Model"""
+
+    @classmethod
+    def setUpClass(cls):
+        """ Connect and load data needed by tests """
+        db.create_all();
+        global ACCOUNT_DATA;
+    
+        with open('tests/fixtures/account_data.json') as json_data:
+            ACCOUNT_DATA = json.load(json_data);
+
+    @classmethod
+    def tearDownClass(cls):
+        """Disconnect from database"""
+        db.session.close();
+
+    def setUp(self):
+        """Truncate the tables"""
+        db.session.query(Account).delete();
+
+    def tearDown(self):
+        """Remove the session"""
+        db.session.remove()
+        
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
+
+    def test_can_create_a_new_account(self):
+        """Test can create new account"""
+        account = Account(**ACCOUNT_DATA[0]);
+        account.create();
+        self.assertEqual(len(Account.all()), 1);
+
+    def test_can_create_all_accounts(self):
+        """Test can create all accounts"""
+        for data in ACCOUNT_DATA:
+            account = Account(**data);
+            account.create();
+        self.assertEqual(len(Account.all()), len(ACCOUNT_DATA));
